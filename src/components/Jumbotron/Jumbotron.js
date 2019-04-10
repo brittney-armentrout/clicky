14c6b2bc085ab749aa78dc113572513207cf4812
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-04-10 12:08:36 -0600

    logo appears

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,10 @@
 import React from "react";
-// import logo from "./logo.png";
+import logo from "../../logo.png";
 
 function Jumbotron () {
     return (
         <div className="jumbotron">
-        <h1>How do i get my logo to show up :( </h1>
-        {/* <img src={logo} alt="Logo" /> */}
+        <img src={logo} alt="Logo" />
         </div>
        );
 };
