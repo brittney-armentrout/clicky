cbd41d51c30be1c0618a1dd82e48badce7a0043e
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-04-10 11:00:18 -0600

    fix app.js

--- src/App.js
+++ src/App.js
@@ -21,61 +21,8 @@ class App extends Component {
     };
   }
 
-
-  //Functions for animation logic
-  // REMOVES animation CSS class
-  removeAnimation = () => {
-    // Grab the text at the top middle of the page
-    let element = document.getElementById("animate-this");
-    // If the guess is correct...
-    if (this.state.isGuessCorrect) {
-      // ...remove the animation style of the correct guess
-      element.classList.remove("jello-vertical");
-    }
-    // If the guess is incorrect...
-    if (!this.state.isGuessCorrect) {
-      // ...remove the animation style of the incorrect guess
-      element.classList.remove("shake-horizontal");
-    }
-  };
-
-  // ADDS animation CSS class
-  addAnimation = isCorrect => {
-    // Grab the text at the top middle of the page
-    let element = document.getElementById("animate-this");
-    // If the guess is correct...
-    if (isCorrect) {
-      // ...add the animation style of the correct guess
-      element.classList.add("jello-vertical");
-    }
-    // If the guess is incorrect...
-    if (!isCorrect) {
-      // ...add the animation style of the incorrect guess
-      element.classList.add("shake-horizontal");
-    }
-  };
-
-  // TOGGLES the CSS class for animation
-  toggleAnimation = isCorrect => {
-    // If the guess is correct...
-    if (isCorrect) {
-      // Add animation
-      this.addAnimation(true);
-      // Wait a split second and then remove it
-      setTimeout(this.removeAnimation, 500);
-    }
-    // If the guess is incorrect...
-    if (!isCorrect) {
-      // Add animation
-      this.addAnimation(false);
-      // Wait a split second and then remove it
-      setTimeout(this.removeAnimation, 500);
-    }
-  };
-
-
   //Functions for game logic
-  // Main click handler function
+  //Main click handler function
   handleSaveClick = id => {
     // Variable to hold the cards state.
     const cardz = this.state.cards;
@@ -88,8 +35,6 @@ class App extends Component {
       cardClicked[0].clicked = true;
       // ...call this function to register the correct guess
       this.handleCorrectClick();
-      // ...add the bouncy animation for correct guess
-      this.toggleAnimation(true);
 
       // ... randomize character cards
       this.randomizeCharacters(cardz);
@@ -97,7 +42,6 @@ class App extends Component {
       this.setState({ cardz });
     } else {
       this.handleIncorrectClick();
-      this.toggleAnimation(false);
     }
   };
 
